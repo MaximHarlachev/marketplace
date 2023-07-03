e7601ebc309bb5a854191680b7bce8d5bbe32bbb
Maxim Kharlachyov <mekcon4ik@mail.ru>
2023-07-03 03:10:15 +0300

    add tests for Order and MarketService

--- src/test/java/ru/inno/market/MarketServiceTest.java
+++ src/test/java/ru/inno/market/MarketServiceTest.java
@@ -1,4 +1,45 @@
 package ru.inno.market;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import ru.inno.market.core.MarketService;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MarketServiceTest {
+    MarketService service;
+    Client client = new Client(1, "Max");
+
+    @BeforeEach
+    public void setUp() {
+        service = new MarketService();
+    }
+
+    @Test
+    @Tag("Bug")
+    @DisplayName("Проверка, что Не создаваётся заказ с пустым клиентом")
+    public void testNotCreateOrderWithNullClient() {
+        assertThrows(NullPointerException.class, () -> service.createOrderFor(null));
+    }
+
+    @Test
+    @DisplayName("Проверка создания заказа для клиента")
+    public void testCreateOrderForClient() {
+        int orderId = service.createOrderFor(client);
+        Order order = service.getOrderInfo(orderId);
+        assertEquals(orderId, order.getId());
+        assertEquals(client, order.getClient());
+    }
+
+    @Test
+    @DisplayName("Проверка, что создаются разные заказы для одного клиента")
+    public void testCreateDifferentOrdersForAClient() {
+        int idOrder1 = service.createOrderFor(client);
+        int idOrder2 = service.createOrderFor(client);
+        assertNotEquals(idOrder1, idOrder2);
+    }
 }
--- src/test/java/ru/inno/market/OrderTest.java
+++ src/test/java/ru/inno/market/OrderTest.java
@@ -1,4 +1,57 @@
 package ru.inno.market;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.inno.market.core.Catalog;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
+import ru.inno.market.model.Order;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OrderTest {
+    Order order;
+    final int orderId = 1;
+    final Client client = new Client(1, "Max");
+    Catalog catalog = new Catalog();
+
+    @BeforeEach
+    public void setUp(){
+        order = new Order(this.orderId, this.client);
+    }
+
+    @Test
+    @DisplayName("Проверка id заказа")
+    public void testGetId() {
+        assertEquals(orderId, order.getId());
+    }
+
+    @Test
+    @DisplayName("Проверка клиента заказа")
+    public void testGetClient() {
+        assertEquals(client, order.getClient());
+    }
+
+    @Test
+    @DisplayName("Проверка, что корзина пуста")
+    public void testEmptyCart() {
+        assertEquals(0, order.getCart().size());
+    }
+
+    @Test
+    @DisplayName("Проверка, что нет скидки, пока не добавили товар")
+    public void testNotDiscount() {
+        assertEquals(false, order.isDiscountApplied());
+    }
+
+    @Test
+    @DisplayName("Проверка добавления товара в корзину")
+    public void testAddItem() {
+        Item item = catalog.getItemById(5);
+        order.addItem(item);
+        assertEquals(1, order.getCart().size());
+    }
+
 }
